c5db1be1d746ee85899fed0fdb25cc64f2636b45
0xjeji <0xjeji@gmail.com>
2025-01-17 22:43:29 -0800

    Added swaync config file

--- .config/hypr/scripts/swww_randomize_once.sh
+++ .config/hypr/scripts/swww_randomize_once.sh
@@ -11,11 +11,9 @@ fi
 
 # Edit below to control the images transition
 export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=2
 
 # Select a random image from the specified directory
 img=$(find "$1" -type f | shuf -n 1)
 
 # Set the selected image as the wallpaper using swww
-swww img "$img"
-
+swww img "$img" --transition-type wipe --transition-duration 1.5
